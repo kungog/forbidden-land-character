23da736653e6aea8254c244327597f0202395bfa
Oscar Gustafson <oscar-gustafsson@live.com>
2023-07-20 13:18:53 +0200

    Box, Coin and Condtion compontens.

---
+++ src/lib/components/Box.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	export let handleClick: any, size: 'medium' |'small' = "small";
+</script>
+
+<button on:click={handleClick} class={`btn-${size}`}>
+  <slot />
+</button>
+
+<style lang="scss">
+  %button-shared {
+    background: var(--color-box);
+    color: var(--color-text);
+    border-radius: var(--radius-04);
+    border: none;
+    width: 100%;
+  }
+
+  .btn-small {
+    @extend %button-shared;
+    padding: var(--spacing-08) var(--spacing-14);
+  }
+
+  .btn-medium {
+    @extend %button-shared;
+    padding: var(--spacing-18);
+  }
+</style>
