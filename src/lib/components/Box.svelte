6d7614ab744230819e26828704a113b148d697d4
Oscar Gustafson <oscar-gustafsson@live.com>
2023-07-21 23:13:00 +0200

    Class for box changed and globals added.

--- src/lib/components/Box.svelte
+++ src/lib/components/Box.svelte
@@ -1,27 +1,28 @@
 <script lang="ts">
-	export let handleClick: any, size: 'medium' |'small' = "small";
+	export let handleClick: any,
+		size: 'medium' | 'small' = 'small';
 </script>
 
-<button on:click={handleClick} class={`btn-${size}`}>
-  <slot />
+<button on:click={handleClick} class={`box-btn-${size}`}>
+	<slot />
 </button>
 
 <style lang="scss">
-  %button-shared {
-    background: var(--color-box);
-    color: var(--color-text);
-    border-radius: var(--radius-04);
-    border: none;
-    width: 100%;
-  }
+	%button-shared {
+		background: var(--color-box);
+		color: var(--color-text);
+		border-radius: var(--radius-04);
+		border: none;
+		width: 100%;
+	}
 
-  .btn-small {
-    @extend %button-shared;
-    padding: var(--spacing-08) var(--spacing-14);
-  }
+	.box-btn-small {
+		@extend %button-shared;
+		padding: var(--spacing-08) var(--spacing-14);
+	}
 
-  .btn-medium {
-    @extend %button-shared;
-    padding: var(--spacing-18);
-  }
+	.box-btn-medium {
+		@extend %button-shared;
+		padding: var(--spacing-18);
+	}
 </style>
--- src/lib/components/Dashboard.svelte
+++ src/lib/components/Dashboard.svelte
@@ -1,107 +1,111 @@
 <script lang="ts">
 	import Box from '$lib/components/Box.svelte';
 	import Text from '$lib/components/Text.svelte';
-	import Coin from './Coin.svelte';
-	import Condition from './Condition.svelte';
+	import Coin from './Character/Coin.svelte';
+	import Condition from './Character/Condition.svelte';
+	import Property from './Character/Property.svelte';
 
-	export let 
-    consumables: Character['consumables'],
-    properties: Character['basic_properties'],
-    condition: Character['condition'],
-    critical: Character['critical_injuries'],
-    money: Character['money'],
-    experience: Character['experience'],
-    powerPoints: Character['power_points'];
+	export let consumables: Character['consumables'],
+		properties: Character['basic_properties'],
+		condition: Character['condition'],
+		critical: Character['critical_injuries'],
+		money: Character['money'],
+		experience: Character['experience'],
+		powerPoints: Character['power_points'];
 </script>
 
-
-
 <div class="main">
-  <div class="condition">
-    <Box size='small' handleClick={() => {}}>
-      <div class="flex space-b">
-        <div>
-          {critical ?? 'No critical injuries'}
-        </div>
-        <div>
-          {#each Object.keys(condition) as item}
-            <Condition type={item} isActive={condition[item]} />
-          {/each}
-        </div>
-      </div>
-    </Box>
-  </div>
-	
-  <div class="property">
-		<Box size='small' handleClick={() => {}}>
-      <div class="flex space-b">
-        {properties.map((property) => property.value)}
-      </div>
-    </Box>
-  </div>
+	<div class="condition">
+		<Box size="small" handleClick={() => {}}>
+			<div class="flex space-b align-c">
+				<div class="flex align-c">
+					{critical ?? 'No critical injuries'}
+				</div>
+				<div class="flex align-c">
+					{#each Object.entries(condition) as [key, value]}
+						<Condition type={key} isActive={value} />
+					{/each}
+				</div>
+			</div>
+		</Box>
+	</div>
 
-  <div class="consumable">
-    <Box size='small' handleClick={() => {}}>
-      <div class="flex space-b">
-        <Text>T{consumables.arrows}</Text>
-        <Text>T{consumables.food}</Text>
-        <Text>T{consumables.torches}</Text>
-        <Text>T{consumables.water}</Text>
-      </div>
-    </Box>
-  </div>
+	<div class="property">
+		<Box size="small" handleClick={() => {}}>
+			<div class="flex space-b align-c">
+				{#each properties as property}
+					<Property type={property.id} isActive={false} />
+				{/each}
+			</div>
+		</Box>
+	</div>
 
-  <div class="point">
-    <Box size='small' handleClick={() => {}}>
-      <div class="flex space-b">
-        <div class="flex space-b">
-          <Text>EP</Text><Text>{experience} / 25</Text>
-        </div>
-        <div class="flex space-b">
-          <Text>KP</Text><Text>{powerPoints} / 10</Text>
-        </div>
-      </div>
-    </Box>
-  </div>
+	<div class="consumable">
+		<Box size="small" handleClick={() => {}}>
+			<div class="flex space-b align-c">
+				<Text>T{consumables.arrows}</Text>
+				<Text>T{consumables.food}</Text>
+				<Text>T{consumables.torches}</Text>
+				<Text>T{consumables.water}</Text>
+			</div>
+		</Box>
+	</div>
 
-  <div class="money">
-		<Box size='small' handleClick={() => {}}>
-      <div class="flex space-b">
-				<Coin type="gold" amount={money.gold}/>
-				<Coin type="silver" amount={money.silver}/>
-				<Coin type="copper" amount={money.copper}/>
-      </div>
-    </Box>
-  </div>
+	<div class="point">
+		<Box size="small" handleClick={() => {}}>
+			<div class="flex space-b align-c">
+				<div class="flex space-b">
+					<Text>EP</Text><Text>{experience} / 25</Text>
+				</div>
+				<div class="flex space-b">
+					<Text>KP</Text><Text>{powerPoints} / 10</Text>
+				</div>
+			</div>
+		</Box>
+	</div>
+
+	<div class="money">
+		<Box size="small" handleClick={() => {}}>
+			<div class="flex space-b align-c">
+				<Coin type="gold" amount={money.gold} />
+				<Coin type="silver" amount={money.silver} />
+				<Coin type="copper" amount={money.copper} />
+			</div>
+		</Box>
+	</div>
 </div>
 
 <style lang="scss">
-  .main {
-    display: grid;
-    grid-template-rows: 1fr 1fr 1fr 1fr;
-    gap: var(--spacing-08);
-    grid-template-areas: 
-      "condition condition condition condition"
-      "properties properties consumables consumables"
-      "points points money money";
-  }
-  .money, .point, .property, .consumable, .condition {
-    width: 100%;
-  }
+	.main {
+		display: grid;
+		grid-template-rows: 1fr 1fr 1fr 1fr;
+		gap: var(--spacing-08);
+		grid-template-areas:
+			'condition condition condition condition'
+			'properties properties consumables consumables'
+			'points points money money';
+	}
+	.money,
+	.point,
+	.property,
+	.consumable,
+	.condition {
+		width: 100%;
+	}
 
-  .money {
-    grid-area: money;
-  }
-  .point {
-    grid-area: points;
-  }
-  .property {
-    grid-area: properties;
-  }
-  .consumable {
-    grid-area: consumables;
-  }
-  .condition {
-    grid-area: condition;
-  }
+	.money {
+		grid-area: money;
+	}
+	.point {
+		grid-area: points;
+	}
+	.property {
+		grid-area: properties;
+	}
+	.consumable {
+		grid-area: consumables;
+	}
+	.condition {
+		grid-area: condition;
+	}
 </style>
