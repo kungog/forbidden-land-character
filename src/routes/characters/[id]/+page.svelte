06713da4bfd1ae1ba0bde84552cdc4b13fbe007c
Oscar Gustafsson <oscar-gustafsson@live.com>
2023-07-29 14:22:59 +0200

    Removal of unecessary props.

--- src/routes/characters/[id]/+page.svelte
+++ src/routes/characters/[id]/+page.svelte
@@ -1,32 +1,12 @@
 <script lang="ts">
-	import { page } from '$app/stores';
-	import CharacterInventory from '$lib/components/CharacterInventory.svelte';
-	import Dashboard from '$lib/components/Character/Dashboard/index.svelte';
-	import Menu from '$lib/components/Menu/Menu.svelte';
 	import type { PageData } from './$types';
+	import Dashboard from '$lib/components/Character/Dashboard.svelte';
+	import Menu from '$lib/components/Menu/Menu.svelte';
+	import Base from '$lib/components/Character/Base.svelte';
 	export let data: PageData;
 	console.log('DATA ---->', data.character);
-
-	const character = data.character;
 </script>
 
-<Dashboard
-	consumables={character.consumables}
-	properties={character.basic_properties}
-	condition={character.condition}
-	critical={character.critical_injuries}
-	money={character.money}
-	experience={character.experience}
-	powerPoints={character.power_points}
-/>
+<Dashboard />
 <Menu />
-<CharacterInventory
-	armor={character.armor}
-	animals={character.animals}
-	notes={character.notes}
-	inventory={character.inventory}
-	relations={character.relations}
-	skills={character.skills}
-	weapons={character.weapons}
-	talents={character.talents}
-/>
+<Base />
