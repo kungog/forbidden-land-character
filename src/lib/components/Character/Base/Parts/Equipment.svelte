d41f3b008e18255a7ae0c4f3c91480948a02d1a8
Oscar Gustafsson <oscar-gustafsson@live.com>
2023-07-30 23:08:25 +0200

    Equipment and Weapon part.

---
+++ src/lib/components/Character/Base/Parts/Equipment.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import Text from '$lib/components/Text.svelte';
+	import BodyIcon from '../../Icons/General/BodyIcon.svelte';
+	import HelmetIcon from '../../Icons/General/HelmetIcon.svelte';
+	import ShieldIcon from '../../Icons/General/ShieldIcon.svelte';
+
+	export let name: string, value: number, icon: 'shield' | 'body' | 'head';
+
+	const equipmentIcon = {
+		head: HelmetIcon,
+		body: BodyIcon,
+		shield: ShieldIcon
+	};
+</script>
+
+<div class="flex equipment">
+	<div class="flex">
+		<svelte:component this={equipmentIcon[icon]} />
+		<Text>{name ?? ''}</Text>
+	</div>
+	<Text>{value ?? 0}</Text>
+</div>
+
+<style lang="scss">
+	div {
+		&.equipment:not(:last-child) {
+			margin-bottom: var(--spacing-10);
+		}
+
+		> div {
+			gap: var(--spacing-10);
+			&:first-child {
+				margin-right: auto;
+			}
+		}
+	}
+</style>
---
+++ src/lib/components/Character/Base/Parts/Weapon.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import Text from '$lib/components/Text.svelte';
+	import WeaponBonusIcon from '../../Icons/General/WeaponBonusIcon.svelte';
+	import WeaponDamageIcon from '../../Icons/General/WeaponDamageIcon.svelte';
+	import WeaponLengthIcon from '../../Icons/General/WeaponLengthIcon.svelte';
+
+	export let additionals: string, bonus: string, damage: string, range: string, type: string;
+</script>
+
+<div class="weapon">
+	<div class="flex space-b">
+		<Text size="small">{type}</Text>
+		<Text size="small">{additionals ?? ''}</Text>
+	</div>
+
+	<div class="flex stats">
+		<Text size="small">Bonus: {bonus}</Text>
+
+		<Text size="small">Skada: {damage}</Text>
+
+		<Text size="small">Räckvidd: {range.toLocaleLowerCase()}</Text>
+	</div>
+</div>
+
+<style lang="scss">
+	.stats {
+		gap: var(--spacing-18);
+	}
+	.weapon > :first-child {
+		margin-bottom: 10px;
+	}
+</style>
