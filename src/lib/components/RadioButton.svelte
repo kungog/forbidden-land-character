a330b2ca6b5e2029e61bbe1d2b013ada72f77787
Oscar Gustafsson <oscar-gustafsson@live.com>
2023-08-05 15:06:46 +0200

    Radio buttons added.

---
+++ src/lib/components/RadioButton.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	export let iValue: number, iFor: string;
+</script>
+
+<div class="radio">
+	{#each [1, 2, 3, 4, 5] as item}
+		<div>
+			<input type="radio" name={iFor} value={item} checked={iValue === item} />
+			<label for={iFor}>{item}</label>
+		</div>
+	{/each}
+</div>
+
+<style lang="scss">
+	.radio {
+		display: flex;
+		gap: var(--spacing-10);
+		margin-top: var(--spacing-10);
+
+		input {
+			opacity: 0;
+			position: absolute;
+			width: 40px;
+			height: 40px;
+			margin: 0;
+		}
+
+		label {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			width: 40px;
+			height: 40px;
+			border-radius: 999px;
+			cursor: pointer;
+			border: 1px solid var(--color-text);
+			color: var(--color-text);
+		}
+
+		input[type='radio']:checked + label {
+			background-color: var(--color-accent);
+			border: 1px solid transparent;
+			color: var(--color-black);
+		}
+	}
+</style>
