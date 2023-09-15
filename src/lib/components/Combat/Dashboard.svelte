aa1cff1c1b82119b19fa137965928ed8de09c7dd
Oscar Gustafsson <oscar-gustafsson@live.com>
2023-09-15 20:38:54 +0200

    Combat dashboard basics, Combat armor.

--- src/lib/components/Combat/Dashboard.svelte
+++ src/lib/components/Combat/Dashboard.svelte
@@ -1,18 +1,28 @@
 <script lang="ts">
+	import Armor from './Dashboard/Armor.svelte';
+	import Conditions from './Dashboard/Conditions.svelte';
+	import Misc from './Dashboard/Misc.svelte';
+	import Weapons from './Dashboard/Weapons.svelte';
 </script>
 
-<div>Combat</div>
+<div>
+	<Conditions />
+	<Weapons />
+	<Armor />
+	<Misc />
+</div>
 
 <style lang="scss">
 	div {
 		display: grid;
 		grid-template-rows: auto;
 		gap: var(--spacing-08);
-		grid-template-columns: repeat(4, 1fr);
+		grid-template-columns: repeat(5, 1fr);
 		grid-template-areas:
-			'condition condition condition condition'
-			'properties properties consumables consumables'
-			'points points money money';
+			'condition condition condition condition condition'
+			'misc misc weapons weapons weapons'
+			'armor armor weapons weapons weapons'
+			'armor armor weapons weapons weapons';
 		padding: 0 var(--spacing-10) var(--spacing-24);
 		justify-items: stretch;
 	}
--- src/routes/combat/[id]/+page.svelte
+++ src/routes/combat/[id]/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import Dashboard from '$lib/components/Character/Dashboard.svelte';
+	import Dashboard from '$lib/components/Combat/Dashboard.svelte';
 	import Menu from '$lib/components/Menu/Menu.svelte';
-	import Base from '$lib/components/Character/Base.svelte';
+	import Base from '$lib/components/Combat/Base.svelte';
 	export let data: PageData;
 	console.log('DATA ---->', data.character);
 </script>
 
 <Dashboard />
-<div class="flex">
+<div>
 	<Menu combat />
 	<Base />
 </div>
