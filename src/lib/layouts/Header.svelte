f12b1c79db6508cf2a8e54e394b6a8f06a835864
Oscar Gustafsson <oscar-gustafsson@live.com>
2023-09-16 08:40:47 +0200

    Null check name.

--- src/lib/layouts/Header.svelte
+++ src/lib/layouts/Header.svelte
@@ -2,7 +2,7 @@
 	import { page } from '$app/stores';
 	import Text from '$lib/components/Text.svelte';
 
-	const { name }: Character = $page.data.character;
+	const character: Character = $page.data.character;
 </script>
 
 <header>
@@ -14,7 +14,7 @@
 			/>
 		</svg>
 	</a>
-	<Text size="medium">{name}</Text>
+	<Text size="medium">{character?.name ?? ''}</Text>
 	<button on:click={() => console.log('open modal')}>
 		<svg width="20" height="27" viewBox="0 0 20 27" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path
