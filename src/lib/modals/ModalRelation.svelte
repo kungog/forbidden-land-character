68452d16edf33e2d62caf9db17a0673ea80dd5e0
Oscar Gustafsson <oscar-gustafsson@live.com>
2023-09-16 23:31:55 +0200

    Show relation modal correctly.

--- src/lib/modals/ModalRelation.svelte
+++ src/lib/modals/ModalRelation.svelte
@@ -3,10 +3,11 @@
 	import { language, modal } from '$lib/store';
 	import { GENERAL_LABELS } from '$lib/helpers/constants/languages';
 	import Textarea from '$lib/components/Textarea.svelte';
+	import { typeCheckPost } from '$lib/helpers/utilites';
 
 	const { relations }: Character = $page.data.character;
 	const LABEL = GENERAL_LABELS[$language];
-	const relation = $modal ? relations[$modal?.index] : '';
+	const relation = !typeCheckPost($modal) ? relations[$modal?.index] : '';
 </script>
 
 <Textarea iLabel={LABEL.relation} iValue={relation} iFor="relation" />
