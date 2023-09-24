a9ee24dc2d8440592d7be0bde58568d8c1a03ad8
Oscar Gustafsson <oscar-gustafsson@live.com>
2023-09-24 14:00:58 +0200

    Form submit on new characters refactored.

--- src/routes/characters/+page.server.ts
+++ src/routes/characters/+page.server.ts
@@ -1,7 +1,9 @@
-import { error } from '@sveltejs/kit';
-import { getMongoClient } from '$lib/server/client';
+import { error, json } from '@sveltejs/kit';
+import { getMongoClient, removeObjectId } from '$lib/server/client';
 import type { PageServerLoad } from './$types';
 import { DATABASE, COLLECTION } from '$lib/server/database';
+import { validateAccess } from '$lib/helpers/validate';
+import { emptyCharacter } from '$lib/helpers/getCharacterObject.js';
 
 export const load = (async ({ cookies }) => {
 	const passcode = cookies.get('passcode');
@@ -26,3 +28,22 @@ export const load = (async ({ cookies }) => {
 
 	throw error(404, 'Not found');
 }) satisfies PageServerLoad;
+
+export const actions = {
+	default: async ({ cookies, request }) => {
+		const data = await request.formData();
+		const passcode = cookies.get('passcode');
+		const database = await getMongoClient();
+
+		validateAccess(passcode);
+
+		const body = { ...emptyCharacter, profile_id: passcode, name: data.get('name') };
+
+		const response = await database.db(DATABASE).collection(COLLECTION.CHARACTERS).insertOne(body);
+
+		// close connection
+		database.close();
+
+		return removeObjectId({ _id: response.insertedId, ...body });
+	}
+};
