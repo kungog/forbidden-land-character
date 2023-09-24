acc84121c43f4c9927e79ed5d91f2dc1024cfacc
Oscar Gustafsson <oscar-gustafsson@live.com>
2023-09-24 15:48:08 +0200

    Basic Form attributes when updating db.

---
+++ src/lib/modals/FormAttributes.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { modal } from '$lib/store';
+
+	const { profile_id, _id }: Character = $page.data.character;
+	const objectKey = `${$modal.key}.${$modal.index}`;
+</script>
+
+<input type="hidden" name="profile_id" value={profile_id} />
+<input type="hidden" name="id" value={_id} />
+<input type="hidden" name="objectKey" value={objectKey} />
