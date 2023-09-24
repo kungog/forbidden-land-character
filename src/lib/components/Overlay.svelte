4288033d142535ac3f035783136099d2b6d6ddc1
Oscar Gustafsson <oscar-gustafsson@live.com>
2023-09-24 13:59:55 +0200

    Overlay component.

---
+++ src/lib/components/Overlay.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let handleClick: any
+</script>
+
+<button on:click={handleClick} />
+
+<style lang="scss">
+  button {
+    position: absolute;
+    border: none;
+    padding: 0;
+    margin: 0;
+    height: 100%;
+    width: 100%;
+    background-color: rgba(0, 0, 0, 20%);
+    backdrop-filter: blur(2px);
+    z-index: 10;
+  }
+</style>
