433dd93057f75ecff1e71b7b567a44f9c2f150a3
Oscar Gustafsson <oscar-gustafsson@live.com>
2024-03-30 23:40:29 +0100

    Add more button.

--- src/lib/components/AddMore.svelte
+++ src/lib/components/AddMore.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import Box from '$components/Box.svelte';
 	import Text from '$components/Text.svelte';
+	export let handleClick: any;
 </script>
 
 <div>
-	<Box size="small" handleClick={() => console.log('2')}>
+	<Box size="small" {handleClick}>
 		<Text size="large">+</Text>
 	</Box>
 </div>
