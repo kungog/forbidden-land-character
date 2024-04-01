3bd906f9ee571c9f3f47fa8f9c85d0bd2ccbeba7
Oscar Gustafsson <oscar-gustafsson@live.com>
2024-04-01 12:01:01 +0200

    Post and delete on Inventory.

--- src/routes/characters/[id]/inventory/+page.svelte
+++ src/routes/characters/[id]/inventory/+page.svelte
@@ -8,30 +8,64 @@
 	import Content from '$layouts/Content.svelte';
 	import Coin from '$widgets/Parts/Coin.svelte';
 	import AddMore from '$components/AddMore.svelte';
-	import { addNewItem } from '$helpers/utilites';
+	import { BASE_URL, addNewItem } from '$helpers/utilites';
 	import Modal from '$components/Modal.svelte';
 	import GridTemplate from '$components/GridTemplate.svelte';
 	import Input from '$components/Input.svelte';
+	import { invalidate } from '$app/navigation';
 
 	export let data: { character: Character; talents: Talent[] } & PageData;
-	$: ({ inventory, money } = data.character);
-	$: edit = null as Inventory | null;
-	let showModal = false;
-
 	const LABEL = GENERAL_LABELS[$language];
+	$: ({ inventory, money } = data.character);
+
+	let showModal = false;
+	let edit: Inventory | null = null;
+	let editIndex = 0;
+
+	const onClose = () => {
+		showModal = false;
+	};
+
+	const onSubmit = async () => {
+		if (!edit) return console.log('Error');
+		const updated = inventory.map((_, index: number) => (index === editIndex ? edit : _));
+		await fetch(BASE_URL + data.character._id, {
+			method: 'POST',
+			body: JSON.stringify({ ...data.character, inventory: updated })
+		});
+
+		showModal = false;
+		invalidate('viewed:character');
+	};
+
+	const onDelete = async () => {
+		const updated = inventory.filter((_, index: number) => index !== editIndex);
+		await fetch(BASE_URL + data.character._id, {
+			method: 'POST',
+			body: JSON.stringify({ ...data.character, inventory: updated })
+		});
+
+		showModal = false;
+		invalidate('viewed:character');
+	};
 </script>
 
 {#if showModal && edit}
-	<Modal handleClose={() => (showModal = false)} handleRemove={() => console.log('Delete: ', edit)}>
+	<Modal {onClose} {onDelete} {onSubmit}>
 		<GridTemplate template="1fr">
-			<Input iType="text" iLabel={LABEL.name} iValue={edit.name} iFor="name" />
+			<Input iType="text" iLabel={LABEL.name} bind:iValue={edit.name} iFor="name" />
 		</GridTemplate>
 		<GridTemplate template="1fr 1fr" gap={48}>
-			<Input iType="text" iLabel={LABEL.bonus} iValue={edit.bonus} iFor="bonus" />
-			<Input iType="number" iLabel={LABEL.weight} iValue={edit.weight} iFor="weight" />
+			<Input iType="text" iLabel={LABEL.bonus} bind:iValue={edit.bonus} iFor="bonus" />
+			<Input iType="number" iLabel={LABEL.weight} bind:iValue={edit.weight} iFor="weight" />
 		</GridTemplate>
 		<GridTemplate template="1fr">
-			<Input iType="text" iLabel={LABEL.additionals} iValue={edit.additionals} iFor="additionals" />
+			<Input
+				iType="text"
+				iLabel={LABEL.additionals}
+				bind:iValue={edit.additionals}
+				iFor="additionals"
+			/>
 		</GridTemplate>
 	</Modal>
 {/if}
